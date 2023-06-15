cb1cdd198e4b4fbeaca3a286dd35e23aef94e09f
Your Name <>
2023-06-15 21:58:57 +0200

    add galery lightbox

--- components/Galery/index.tsx
+++ components/Galery/index.tsx
@@ -1,32 +1,67 @@
-import { Container, Grid } from "@mui/material"
-import { GaleryItem, GaleryS } from "./styled"
-import Image from "next/image"
-import { FC } from "react"
+import { Container, Grid } from "@mui/material";
+import { GaleryItem, GaleryS } from "./styled";
+import Image from "next/image";
+import { FC } from "react";
+import "lightgallery.js/dist/css/lightgallery.css";
+// @ts-ignore
+import { LightgalleryProvider, LightgalleryItem } from "react-lightgallery";
 
-const APP_API = process.env.APP_API
+const APP_API = process.env.APP_API;
 
 const grid = (images: IImages) => {
   return (
     <Grid container>
-      {images.data.slice(0, 8).map((item: IImageAttributes, idx: number) => <Grid key={idx} item xs={6} md={3}>
-        <GaleryItem>
-          <Image src={`${APP_API+item.attributes.url}?format=webp&resize=360x285`} fill alt="" />
-        </GaleryItem>
-      </Grid>)}
+      {images.data.slice(0, 8).map((item: IImageAttributes, idx: number) => (
+        <Grid key={idx} item xs={6} md={3}>
+          <LightgalleryItem
+              group="any"
+              component="div"
+              src={APP_API + item.attributes.url}
+            >
+          <GaleryItem>
+            <a
+              href={APP_API + item.attributes.url}
+              onClick={(e) => e.preventDefault()}
+            >
+              <Image
+                src={`${
+                  APP_API + item.attributes.url
+                }?format=webp&resize=360x285`}
+                fill
+                alt=""
+              />
+            </a>
+          </GaleryItem>
+          </LightgalleryItem>
+        </Grid>
+      ))}
     </Grid>
-  )
-}
+  );
+};
 
 const Galery: FC<IGalery> = ({
   modal = false,
   images,
-  removePadding = false
+  removePadding = false,
 }) => {
   return (
-    <GaleryS modal={modal} removePadding={removePadding}>
-      {!modal ? <Container maxWidth="xl">{grid(images)}</Container> : grid(images)}
-    </GaleryS>
-  )
-}
+    <LightgalleryProvider
+      lightgallerySettings={
+        {
+          // settings: https://sachinchoolur.github.io/lightgallery.js/docs/api.html
+        }
+      }
+      galleryClassName="my_custom_classname"
+    >
+      <GaleryS modal={modal} removePadding={removePadding}>
+        {!modal ? (
+          <Container maxWidth="xl">{grid(images)}</Container>
+        ) : (
+          grid(images)
+        )}
+      </GaleryS>
+    </LightgalleryProvider>
+  );
+};
 
-export default Galery
+export default Galery;
