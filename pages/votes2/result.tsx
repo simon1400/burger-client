3c188263e19b4b4f4e6964a2d87e72407ebe10fe
Your Name <>
2024-04-24 00:11:30 +0200

    create another votes

---
+++ pages/votes2/result.tsx
@@ -0,0 +1,153 @@
+import { Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import Page from "layout/Page";
+import { client } from "lib/api";
+import { NextPage } from "next"
+import { getAllVotes } from "queries/votes";
+import { useEffect, useState } from "react";
+
+export const getServerSideProps = (async () => {
+  const { data } = await client.query({
+    query: getAllVotes
+  });
+
+  return { 
+    props: { 
+      data: data.votes.data,
+      votes: true
+    } 
+}
+})
+
+const createData = (
+  festival: string,
+  shop: string,
+  name: string,
+  email: string,
+  phone: string,
+  codes: string,
+) => ({ festival, shop, name, email, phone, codes })
+
+const ResultVotes: NextPage<{data?: any}> = ({data}) => {
+
+  const [hasPassword, setHasPassword] = useState(false)
+  const [score, setScore] = useState([])
+
+  const onlyUnique = (value: string, index: number, array: any) => array.indexOf(value) === index
+
+  const hadnelWinnerTable = (data: any) => {
+    const arrName = data.map((item: any) => item.attributes.shop)
+    var uniqueName = arrName.filter(onlyUnique);
+    // console.log(uniqueName)
+    const filtered: any = []
+    uniqueName.map((item: string, idx: number) => {
+      filtered.push({name: item, codes: 0})
+      for(let i = 0; i < data.length; i++) {
+        if(item === data[i].attributes.shop) {
+          filtered[idx].codes += data[i].attributes.codes.length
+        }
+      }
+    })
+    setScore(filtered.sort((a: any, b:any) => b.codes - a.codes))
+  }
+  
+  useEffect(() => {
+    if(!hasPassword) {
+      const enteredFood = prompt('Please enter password:')
+      if(enteredFood === "f5342wegstse5t3") {
+        setHasPassword(true)
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    hadnelWinnerTable(data)
+  }, [hasPassword])
+
+  if(!hasPassword) {
+    return null
+  }
+
+  const rows = [
+    ...data.map((item: any) => {
+      const dataLocal = item.attributes
+      return createData(
+        dataLocal.festivaly.data.attributes.title, 
+        dataLocal.shop, 
+        dataLocal.name, 
+        dataLocal.email, 
+        dataLocal.phone, 
+        [...dataLocal.codes.map((code: any) => code.code)].join(',')
+      )
+    })
+  ];
+
+  return (
+    <Page>
+      {!!score.length && <Container>
+        <Typography variant="h1" style={{marginBottom: 20, marginTop: 100}}>Tabulka vyhercu</Typography>
+        <TableContainer style={{marginBottom: 100}}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>#</TableCell>
+                <TableCell>Shop</TableCell>
+                <TableCell>Score</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {score.map((row: any, idx: number) => (
+                <TableRow
+                  key={idx}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {idx+1}
+                  </TableCell>
+                  <TableCell>{row.name}</TableCell>
+                  <TableCell><b>{row.codes}</b></TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Container>}
+
+      <Container>
+        <Typography variant="h1" style={{marginBottom: 20}}>Result hlasovani</Typography>
+        <TableContainer>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Festival</TableCell>
+                <TableCell align="right">Shop</TableCell>
+                <TableCell align="right">Name</TableCell>
+                <TableCell align="right">Email</TableCell>
+                <TableCell align="right">Phone</TableCell>
+                <TableCell align="right">Codes</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.festival}
+                  </TableCell>
+                  <TableCell align="right">{row.shop}</TableCell>
+                  <TableCell align="right">{row.name}</TableCell>
+                  <TableCell align="right">{row.email}</TableCell>
+                  <TableCell align="right">{row.phone}</TableCell>
+                  <TableCell align="right">{row.codes}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Container>
+    </Page>
+  )
+}
+
+export default ResultVotes
